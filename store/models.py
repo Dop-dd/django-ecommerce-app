552a3c7d3daeac2e2389a8bc36fa86cd1523c387
Dop <dopddaiga@gmail.com>
2022-12-09 21:19:38 +0100

    added Category and Product models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category, blank=True')
+
+    def __str__(self):
+        return self.name
+
+# product
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    # define a many-to-one relationship
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product, blank=True')
+    # check stock availability
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    # product creation date
+    created = models.DateTimeField(auto_now_add=True)
+    updateed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
